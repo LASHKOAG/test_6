ff0a144f55e1f0215825031fa071aaf528810c3a
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2014-07-09 00:09:40 +0700

    add author

--- Blink_for_LEDshield/Blink_for_LEDshield.ino
+++ Blink_for_LEDshield/Blink_for_LEDshield.ino
@@ -1,3 +1,15 @@
+  /*********************************************************************************
+     * Project  : Blink for LED shield                                             *
+     * Compiler : Arduino 1.0.2                                                    *
+     * Board    : Arduino UNO                                                      *
+     * Shield   : D.I.Y LED Shield 4x3                                             *
+     * Module   :                                                                  *
+     *          :                                                                  *
+     * Author   : Bavensky :3                                                      *
+     * E-Mail   : Aphirak_Sang-ngenchai@hotmail.com                                *
+     * Date     : 09/07/2014 [dd/mm/yyyy]                                          *
+     *******************************************************************************/
+     
   #define LEDPIN2  2
   #define LEDPIN3  3
   #define LEDPIN4  4
@@ -10,17 +22,16 @@
   #define LEDPIN11  11
   #define LEDPIN12  12
   #define LEDPIN13  13
+  #define DELAYTIME 100
   
   int ledpins[] = {  LEDPIN2, LEDPIN3, LEDPIN4, LEDPIN5, 
                      LEDPIN6, LEDPIN7, LEDPIN8, LEDPIN9,
                      LEDPIN10, LEDPIN11, LEDPIN12, LEDPIN13,  };
-                /*{  11, 8, 5, 2, 
-                     12, 9, 6, 3,
-                     13, 10, 7, 4,  };*/
+                     
   void setup() 
   {
-    //Serial.begin(9600);
-    for(int i=0; i<12; i++){
+    for(int i=0; i<12; i++)
+    {
       pinMode(ledpins[i], OUTPUT);
       delay(2);
     }
@@ -32,8 +43,8 @@
     for(int i=0; i<12; i++)
     {
       digitalWrite(ledpins[i], HIGH);
-      delay(100);
+      delay(DELAYTIME);
       digitalWrite(ledpins[i], LOW);
-      delay(100);
+      delay(DELAYTIME);
     }
   }
